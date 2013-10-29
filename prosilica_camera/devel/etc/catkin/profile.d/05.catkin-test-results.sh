555cae9a45466b048badaed736cc4f685664879d
SPN <spn@intenseA.(none)>
2013-10-29 11:15:27 -0400

    RGB24 fix

---
+++ prosilica_camera/devel/etc/catkin/profile.d/05.catkin-test-results.sh
@@ -0,0 +1,4 @@
+# generated from catkin/cmake/env-hooks/05.catkin-test-results.sh.develspace.in
+
+export CATKIN_TEST_RESULTS_DIR="/home/spn/dev/ROS/myclone/prosilica_driver/prosilica_camera/test_results"
+export ROS_TEST_RESULTS_DIR="$CATKIN_TEST_RESULTS_DIR"
